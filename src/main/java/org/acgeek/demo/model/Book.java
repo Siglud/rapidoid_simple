472171137848ed6fd2e85f596708cd7dddbbeaa1
Siglud <Siglud@gmail.com>
2016-06-17 17:15:59 +0800

    add mysql config

--- src/main/java/org/acgeek/demo/model/Book.java
+++ src/main/java/org/acgeek/demo/model/Book.java
@@ -3,9 +3,11 @@ package org.acgeek.demo.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 @Entity
+@Table(name="book")
 public class Book {
     @Id
     @GeneratedValue
